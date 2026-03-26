0f601d71961416e34656b5ab49cddaf5bf7ce9a4
Natan <natancruz19@gmail.com>
2026-03-26 02:21:46 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Projeto com Deschamps através do curso.dev 
+
+Projeto com Deschamps através do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Você é um Chuchu! kkkkkk Linda</h1>
+  return <h1>Você é um Chuchu! kkkkkk Linda</h1>;
 }
 
-export default Home; 
+export default Home;
+q;
