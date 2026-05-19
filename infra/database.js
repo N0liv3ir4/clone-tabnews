cc5dd3753d1e8a0054bf0187cec050d609d13806
Natan <natancruz19@gmail.com>
2026-05-19 23:05:02 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -28,3 +28,16 @@ async function query(queryObject) {
     await client.end();
   }
 }
+
+export default {
+  query: query,
+};
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
